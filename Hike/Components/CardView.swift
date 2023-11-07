8e1353aad007965ffe1680917f8ec27406eafe95
Ivan Trajanovski <ivantrajanovski@gmail.com>
2023-11-07 13:55:05 +0100

    create button and header

--- Hike/Components/CardView.swift
+++ Hike/Components/CardView.swift
@@ -12,16 +12,47 @@ struct CardView: View {
         ZStack {
             CustomBackgroundView()
             
-            ZStack {
-                Circle()
-                    .fill(
-                        LinearGradient(colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")], startPoint: .topLeading, endPoint: .bottomTrailing)
-                    )
-                    .frame(width: 256, height: 256)
+            VStack {
+                // MARK: - HEADER
                 
-                Image("image-1")
-                    .resizable()
-                .scaledToFit()
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.bold)
+                            .font(.system(size: 52))
+                            .foregroundStyle(
+                                LinearGradient(colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom)
+                        )
+                        
+                        Spacer()
+                        
+                        Button {
+                            print("button was pressed")
+                        } label: {
+                            CustomButtonView()
+                        }
+                    }
+                    
+                    Text("Fun and enjoyable outdoor activity.")
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                }
+                .padding(.horizontal, 30)
+                
+                //MARK: - MAIN CONTENT
+                
+                
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")], startPoint: .topLeading, endPoint: .bottomTrailing)
+                        )
+                        .frame(width: 256, height: 256)
+                    
+                    Image("image-1")
+                        .resizable()
+                    .scaledToFit()
+                }
             }
         }
         .frame(width: 320, height: 570)
