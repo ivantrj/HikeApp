63c210cac4c1fb8b3a633412be56068d2c1f579f
Ivan Trajanovski <ivantrajanovski@gmail.com>
2023-11-07 11:50:16 +0100

    first commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by ivan ruwido  on 07.11.23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
